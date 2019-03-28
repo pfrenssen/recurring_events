57de9bf8ff444499ddbfc81dbf83d0330c378197
Owen Bush <ojb@ukhhf.co.uk>
2019-03-27 23:04:15 -0600

    Added filter and sorts for event instance computed fields

---
+++ src/Plugin/views/filter/EventInstanceDescription.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\recurring_events\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\StringFilter;
+
+/**
+ * Filter handler for the inherited event instance description.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("eventinstance_description")
+ */
+class EventInstanceDescription extends StringFilter {
+
+}
---
+++ src/Plugin/views/sort/EventInstanceTitle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\recurring_events\Plugin\views\sort;
+
+use Drupal\views\Plugin\views\sort\Standard;
+
+/**
+ * Sort handler for the inherited event instance title.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsSort("eventinstance_title")
+ */
+class EventInstanceTitle extends Standard {
+
+}
