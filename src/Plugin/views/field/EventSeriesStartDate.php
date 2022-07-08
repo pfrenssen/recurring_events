2c4a6d806353df908f6d09bd542737ea14974457
Matroskeen <matroskeen@3426249.no-reply.drupal.org>
2022-07-08 21:12:20 +0000

    Issue #3283128 by Matroskeen, owenbush: Use date.formatter service to format date values

--- src/Plugin/views/field/EventSeriesStartDate.php
+++ src/Plugin/views/field/EventSeriesStartDate.php
@@ -4,6 +4,7 @@ namespace Drupal\recurring_events\Plugin\views\field;
 
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field handler to show the start date of an event series.
@@ -13,6 +14,22 @@ use Drupal\views\ResultRow;
  * @ViewsField("eventseries_start_date")
  */
 class EventSeriesStartDate extends FieldPluginBase {
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->dateFormatter = $container->get('date.formatter');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -33,7 +50,7 @@ class EventSeriesStartDate extends FieldPluginBase {
 
     if (!empty($event_start)) {
       $format = \Drupal::config('recurring_events.eventseries.config')->get('date_format');
-      $start_date = $event_start->setTimezone($timezone)->format($format);
+      $start_date = $this->dateFormatter->format($event_start->getTimestamp(), 'custom', $format, $timezone->getName());
     }
     return $start_date;
   }
