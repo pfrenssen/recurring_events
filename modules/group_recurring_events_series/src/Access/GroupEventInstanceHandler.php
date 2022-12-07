ff6e3539bf82677eb5dc3b601085ab073484e12c
Rotekian <61598-Rotekian@users.noreply.drupalcode.org>
2022-12-07 21:56:03 +0000

    Issue #3321269: Replacement access handler for EventInstance.

---
+++ modules/group_recurring_events_series/src/Access/GroupEventInstanceHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\group_recurring_events_series\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+use Drupal\recurring_events\Entity\EventSeries;
+use Drupal\recurring_events\EventInstanceAccessControlHandler;
+
+class GroupEventInstanceHandler extends EventInstanceAccessControlHandler {
+    protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+        $result = parent::checkAccess($entity, $operation, $account);
+        if(!$result->isAllowed()) {
+            // Need GroupContentAccessControlHandler not EventSeriesAccessControlHandler
+            $manager = \Drupal::service('plugin.manager.group_content_enabler');
+            $type = 'group_recurring_events_series:' . $entity->getType();
+            if($manager->hasHandler($type, 'access')) {
+              $handler = $manager->getAccessControlHandler($type);
+              $result = $handler->entityAccess($entity->getEventSeries(), $operation, $account, TRUE);
+            }
+        }
+        return $result;
+    }
+}
