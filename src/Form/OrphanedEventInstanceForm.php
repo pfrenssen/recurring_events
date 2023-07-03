d4543dda2c472958f5b4e4442dba344088f1b1ff
Oleg Rymar <56919-oleg_rymar@users.noreply.drupalcode.org>
2023-07-03 12:00:53 +0300

    Issue #3370209 by olegrymar: Error: Call to undefined method Drupal\mysql\Driver\Database\mysql\Select::accessCheck()

--- src/Form/OrphanedEventInstanceForm.php
+++ src/Form/OrphanedEventInstanceForm.php
@@ -156,7 +156,6 @@ class OrphanedEventInstanceForm extends FormBase {
       ->fields('efd', ['id']);
     $query->leftJoin('eventseries', 'es', 'efd.eventseries_id = es.id');
     $instances = $query->condition('es.id', NULL, 'IS NULL')
-      ->accessCheck(FALSE)
       ->execute()
       ->fetchCol();
 
