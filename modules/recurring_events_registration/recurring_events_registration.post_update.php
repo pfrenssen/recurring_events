6e5921c38e484552280517a7497cfcd3179d77a3
Pieter Frenssen <pieter@frenssen.be>
2024-09-27 17:58:15 +0300

    Store overridden notification settings per registrant type.

--- modules/recurring_events_registration/recurring_events_registration.post_update.php
+++ modules/recurring_events_registration/recurring_events_registration.post_update.php
@@ -7,6 +7,7 @@
 
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\recurring_events_registration\Model\RegistrantTypeNotificationSetting;
 
 /**
  * Update registrant to be revisionable.
@@ -89,4 +90,20 @@ function recurring_events_registration_post_update_make_registrant_revisionable(
   $definition_update_manager->updateFieldableEntityType($entity_type, $field_storage_definitions, $sandbox);
 
   return t('Registrants have been converted to be revisionable.');
+}
+
+/**
+ * Add per-registrant type notification settings.
+ */
+function recurring_events_registration_post_update_add_notifications_to_registrant_types(&$sandbox) {
+  $registrant_types = \Drupal::entityTypeManager()
+    ->getStorage('registrant_type')
+    ->loadMultiple();
+
+  foreach ($registrant_types as $registrant_type) {
+    // Resave the entity to ensure the notification settings are saved.
+    $registrant_type
+      ->setNotificationSettings($registrant_type->getNotificationSettings())
+      ->save();
+  }
 }
