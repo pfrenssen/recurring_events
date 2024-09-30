01bcc97ab77d2842f132d1ff2513563020321810
Pieter Frenssen <pieter@frenssen.be>
2024-09-30 09:12:55 +0300

    Start on a test.

---
+++ modules/recurring_events_registration/tests/src/Kernel/NotificationTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\recurring_events_registration\Kernel;
+
+use Drupal\Core\Test\AssertMailTrait;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\recurring_events_registration\Entity\Registrant;
+use Drupal\recurring_events_registration\Entity\RegistrantType;
+use Drupal\recurring_events_registration\Model\RegistrantTypeNotificationSetting;
+use Drupal\Tests\recurring_events\Traits\EventSeriesCreationTrait;
+
+/**
+ * Tests the notifications sent during the event registration process.
+ *
+ * @group recurring_events_registration
+ */
+class NotificationTest extends KernelTestBase {
+
+  use AssertMailTrait;
+  use EventSeriesCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'datetime',
+    'datetime_range',
+    'field',
+    'field_inheritance',
+    'options',
+    'recurring_events',
+    'recurring_events_registration',
+    'system',
+    'text',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+    $this->installEntitySchema('eventinstance');
+    $this->installEntitySchema('eventseries');
+    $this->installEntitySchema('registrant');
+    $this->installEntitySchema('user');
+    $this->installConfig(['system', 'recurring_events_registration']);
+  }
+
+  /**
+   * Tests the `registration_notification` notification.
+   */
+  public function testRegistrationNotification(): void {
+    $config = \Drupal::config('recurring_events_registration.registrant.config');
+    $send_email = $config->get('email_notifications');
+    $this->assertTrue($send_email);
+    $send_email_key = $config->get('notifications.registration_notification.enabled');
+    $this->assertTrue($send_email_key);
+    $event_series = $this->createEventSeries();
+
+    // Create a registrant type 'group' which overrides the registration
+    // notification.
+    $registrant_type = RegistrantType::create([
+      'id' => 'group',
+      'label' => 'Group',
+    ]);
+    $registrant_type->setNotificationSettings([
+      'registration_notification' => new RegistrantTypeNotificationSetting([
+        'enabled' => TRUE,
+        'subject' => 'Your group has been registered',
+        'body' => 'Thank you [] for registering your group for the event.',
+      ]),
+    ]);
+    $registrant_type->save();
+
+    // Create a registrant. This should send out a notification.
+    $registrant = Registrant::create([
+      'bundle' => 'group',
+      'eventseries_id' => $event_series->id(),
+      'type' => 'series',
+      'field_first_name' => 'Kris',
+      'status' => TRUE,
+    ]);
+    $registrant->save();
+  }
+
+}
---
+++ tests/src/Traits/EventSeriesCreationTrait.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\recurring_events\Traits;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\recurring_events\Entity\EventSeries;
+use Drupal\TestTools\Random;
+
+/**
+ * Helper methods for creating event series entities.
+ *
+ * This trait is meant to be used only by test classes.
+ */
+trait EventSeriesCreationTrait {
+
+  /**
+   * Creates an event series.
+   *
+   * @param array $values
+   *   Optional field values to set on the event series.
+   *
+   * @return \Drupal\recurring_events\Entity\EventSeries
+   *   The event series entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   When the creation of the event series entity failed.
+   */
+  protected function createEventSeries(array $values = []): EventSeries {
+    // Provide default values for required fields.
+    $values += [
+      'name' => Random::string(),
+      'status' => TRUE,
+      'type' => 'default',
+      'recur_type' => 'weekly_recurring_date',
+    ];
+
+    if ($values['recur_type'] === 'weekly_recurring_date') {
+      $values += [
+        'weekly_recurring_date' => [
+          'start_date' => new DrupalDateTime('2024-05-19T15:00:00'),
+          'end_date' => new DrupalDateTime('2024-06-09T15:00:00'),
+          'time' => '03:00 pm',
+          'duration' => '3600',
+          'end_time' => '04:00 pm',
+          'duration_or_end_time' => 'duration',
+          'days' => 'sunday',
+        ],
+      ];
+    }
+
+    $series = EventSeries::create($values);
+    $series->save();
+
+    return $series;
+  }
+
+}
